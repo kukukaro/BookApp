d16caf7c8b6920d1472470e31dd85870080f663d
Karolina Kukula <kukukaro@gmail.com>
2021-01-16 20:45:11 +0100

    Refactor register selenium test

--- src/test/java/selenium/user/RegisterTest.java
+++ src/test/java/selenium/user/RegisterTest.java
@@ -1,6 +1,8 @@
 package selenium.user;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,136 +14,76 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RegisterTest {
 
+    WebDriver driver;
+
+    @Before
+    public void startDriver() {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://ta-ebookrental-fe.herokuapp.com/register");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 
     @Test
     public void registerWithValidData() {
-        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\Chrome\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://ta-ebookrental-fe.herokuapp.com/register");
 
         String userName = RandomStringUtils.randomAlphabetic(10);
         String password = "Karo1";
 
-        WebElement inputLogin = driver.findElement(By.cssSelector("input[name=\"login\"]"));
-        inputLogin.sendKeys(userName);
-
-        WebElement inputPassword = driver.findElement(By.cssSelector("input[name=\"password\"]"));
-        inputPassword.sendKeys(password);
-
-        WebElement inputConfirmPassword = driver.findElement(By.cssSelector("input[name=\"password-repeat\"]"));
-        inputConfirmPassword.sendKeys(password);
-
-        WebElement signUpButton = driver.findElement(By.cssSelector("button[id=\"register-btn\"]"));
-        signUpButton.click();
-
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(webDriver -> driver.findElement(By.cssSelector("p[class=\"alert__content\"]")));
-
-
         String correctRegisterConfirmation = "You have been successfully registered!";
-
-        WebElement registerPassed = driver.findElement(By.cssSelector("p[class=\"alert__content\"]"));
-        String registerPassedConfirmation = registerPassed.getText();
-
-        System.out.println(registerPassedConfirmation);
+        String registerPassedConfirmation = fillRegisterFormAndSend(userName, password, password);
 
         boolean isPassed = correctRegisterConfirmation.equals(registerPassedConfirmation);
-        System.out.println(isPassed);
         assertThat(isPassed).isTrue();
-
-        driver.quit();
     }
 
     @Test
     public void registerWithEmptyLogin() {
-        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\Chrome\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://ta-ebookrental-fe.herokuapp.com/register");
 
         String userName = "";
         String password = "Karo1";
 
-        WebElement inputLogin = driver.findElement(By.cssSelector("input[name=\"login\"]"));
-        inputLogin.sendKeys(userName);
-
-        WebElement inputPassword = driver.findElement(By.cssSelector("input[name=\"password\"]"));
-        inputPassword.sendKeys(password);
-
-        WebElement inputConfirmPassword = driver.findElement(By.cssSelector("input[name=\"password-repeat\"]"));
-        inputConfirmPassword.sendKeys(password);
-
-        WebElement signUpButton = driver.findElement(By.cssSelector("button[id=\"register-btn\"]"));
-        signUpButton.click();
-
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(webDriver -> driver.findElement(By.cssSelector("p[class=\"alert__content\"]")));
-
-
         String incorrectRegisterConfirmation = "You can't leave fields empty";
-
-        WebElement registerFailed = driver.findElement(By.cssSelector("p[class=\"alert__content\"]"));
-        String registerPassedConfirmation = registerFailed.getText();
-
-        System.out.println(registerPassedConfirmation);
+        String registerPassedConfirmation = fillRegisterFormAndSend(userName, password, password);
 
         boolean isPassed = incorrectRegisterConfirmation.equals(registerPassedConfirmation);
-        System.out.println(isPassed);
         assertThat(isPassed).isTrue();
-
-        driver.quit();
     }
 
     @Test
     public void registerWithEmptyPassword() {
-        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\Chrome\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://ta-ebookrental-fe.herokuapp.com/register");
 
         String userName = RandomStringUtils.randomAlphabetic(10);
         String password = "";
 
-        WebElement inputLogin = driver.findElement(By.cssSelector("input[name=\"login\"]"));
-        inputLogin.sendKeys(userName);
-
-        WebElement inputPassword = driver.findElement(By.cssSelector("input[name=\"password\"]"));
-        inputPassword.sendKeys(password);
-
-        WebElement inputConfirmPassword = driver.findElement(By.cssSelector("input[name=\"password-repeat\"]"));
-        inputConfirmPassword.sendKeys(password);
-
-        WebElement signUpButton = driver.findElement(By.cssSelector("button[id=\"register-btn\"]"));
-        signUpButton.click();
-
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(webDriver -> driver.findElement(By.cssSelector("p[class=\"alert__content\"]")));
-
-
         String incorrectRegisterConfirmation = "You can't leave fields empty";
-
-        WebElement registerFailed = driver.findElement(By.cssSelector("p[class=\"alert__content\"]"));
-        String registerPassedConfirmation = registerFailed.getText();
-
-        System.out.println(registerPassedConfirmation);
+        String registerPassedConfirmation = fillRegisterFormAndSend(userName, password, password);
 
         boolean isPassed = incorrectRegisterConfirmation.equals(registerPassedConfirmation);
-        System.out.println(isPassed);
         assertThat(isPassed).isTrue();
-
-        driver.quit();
     }
 
     @Test
     public void registerWithNotMatchingPassword() {
-        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\Chrome\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://ta-ebookrental-fe.herokuapp.com/register");
 
         String userName = RandomStringUtils.randomAlphabetic(10);
         String password = RandomStringUtils.randomAlphabetic(10);
         String password2 = RandomStringUtils.randomAlphabetic(10);
 
+        String incorrectRegisterConfirmation = "The passwords don't match";
+        String registerPassedConfirmation = fillRegisterFormAndSend(userName, password, password2);
+
+        boolean isPassed = incorrectRegisterConfirmation.equals(registerPassedConfirmation);
+        assertThat(isPassed).isTrue();
+    }
+
+    private String fillRegisterFormAndSend(String login, String password, String password2) {
         WebElement inputLogin = driver.findElement(By.cssSelector("input[name=\"login\"]"));
-        inputLogin.sendKeys(userName);
+        inputLogin.sendKeys(login);
 
         WebElement inputPassword = driver.findElement(By.cssSelector("input[name=\"password\"]"));
         inputPassword.sendKeys(password);
@@ -155,18 +97,7 @@ public class RegisterTest {
         WebDriverWait wait = new WebDriverWait(driver, 5);
         wait.until(webDriver -> driver.findElement(By.cssSelector("p[class=\"alert__content\"]")));
 
-
-        String incorrectRegisterConfirmation = "The passwords don't match";
-
         WebElement registerFailed = driver.findElement(By.cssSelector("p[class=\"alert__content\"]"));
-        String registerPassedConfirmation = registerFailed.getText();
-
-        System.out.println(registerPassedConfirmation);
-
-        boolean isPassed = incorrectRegisterConfirmation.equals(registerPassedConfirmation);
-        System.out.println(isPassed);
-        assertThat(isPassed).isTrue();
-
-        driver.quit();
+        return registerFailed.getText();
     }
 }
