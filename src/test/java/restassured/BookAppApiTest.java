4a57ef4a498aab9000e230f3a1c6332f9ac2a69f
Karolina Kukula <kukukaro@gmail.com>
2021-02-14 18:06:04 +0100

    Delete book tests

--- src/test/java/restassured/BookAppApiTest.java
+++ src/test/java/restassured/BookAppApiTest.java
@@ -40,4 +40,28 @@ public abstract class BookAppApiTest {
         assertThat(userId).isPositive();
         return userId;
     }
+
+    protected int addBook(String title, String author, int year, int userId) {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("year", year);
+        requestBody.put("author", author);
+        requestBody.put("title", title);
+        requestBody.put("userId", userId);
+
+        Response respond = RestAssured
+                .given()
+                .basePath("titles/")
+                .contentType(ContentType.JSON)
+                .body(requestBody.toString())
+                .log().all()
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .and()
+                .statusCode(200)
+                .extract().response();
+
+        return respond.getBody().as(Integer.class);
+    }
 }
--- src/test/java/restassured/title/DeleteBookTest.java
+++ src/test/java/restassured/title/DeleteBookTest.java
@@ -1,12 +1,92 @@
 package restassured.title;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Ignore;
 import org.junit.Test;
 import restassured.BookAppApiTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeleteBookTest extends BookAppApiTest {
 
     @Test
     public void shouldDeleteBook() {
 
+        int userId = logInSuccessfully();
+        int bookId = addBook("TitleTest", "AuthorTest", 2020, userId);
+
+        Response response = RestAssured
+                .given()
+                .basePath("titles/")
+                .queryParam("userId", userId)
+                .queryParam("id", bookId)
+                .when()
+                .delete()
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        boolean isNew = response.getBody().as(Boolean.class);
+
+        assertThat(isNew).isTrue();
     }
+
+    /*Delete book from not logged user should not be allowed.
+    Instead the books was deleted.  */
+    @Ignore
+    @Test
+    public void shouldNotDeleteBookWithoutLog() {
+        int userId = 11414;
+        int bookId = addBook("TitleTest", "AuthorTest", 2020, userId);
+
+        RestAssured
+                .given()
+                .basePath("titles/")
+                .queryParam("userId", userId)
+                .queryParam("id", bookId)
+                .when()
+                .delete()
+                .then()
+                .statusCode(400)
+                .extract().response().prettyPrint();
+    }
+
+    @Test
+    public void shouldNotDeleteBookWithoutUserId() {
+
+        int userId = logInSuccessfully();
+        int bookId = addBook("TitleTest", "AuthorTest", 2020, userId);
+
+        RestAssured
+                .given()
+                .basePath("titles/")
+                .queryParam("userId", "")
+                .queryParam("id", bookId)
+                .when()
+                .delete()
+                .then()
+                .statusCode(400)
+                .extract().response().prettyPrint();
+    }
+
+    @Test
+    public void shouldNotDeleteBookWithoutBookId() {
+
+        int userId = logInSuccessfully();
+        int bookId = addBook("TitleTest", "AuthorTest", 2020, userId);
+
+        RestAssured
+                .given()
+                .basePath("titles/")
+                .queryParam("userId", userId)
+                .queryParam("id", "")
+                .when()
+                .delete()
+                .then()
+                .statusCode(400)
+                .extract().response().prettyPrint();
+
+    }
+
 }
